0a33e0512e8e08955b140e038c35cad8fa152e13
Raj Shrimali <rajshrimali@gmail.com>
2016-10-24 19:20:42 -0400

    include my auth

--- producer.py
+++ producer.py
@@ -3,11 +3,10 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-
-access_token = ""
-access_token_secret = ""
-consumer_key = ""
-consumer_secret = ""
+access_token = "3344745593-v2hyQ5BXJd67iDyBEwFBlCMRJ8uPuYAeMmOkSxt"
+access_token_secret = "j7S0Pi5Xusl1Tftna5cCb0EtgLTBanuE5UnMwGFC3DqI2"
+consumer_key = "rzVhEV6XGLGOUrrvq6hxpS38m"
+consumer_secret = "3CXsrtozZE0a7AFKva7FDqT6exBt42bR7gvZdBuH0pp2rXr9B7"
 
 
 class StreamProducer(StreamListener):
